84ac66aad78ded76cc5cfab29541e9415960591e
Alysia Lynn <itsalysialynn@gmail.com>
2017-06-06 15:37:46 -0700

    added validiations

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,17 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+class Employee < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: { greater_than: 40, less_than: 200 }
+  validates :store_id, presence: true
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, length: { greater_than_or_equal_to: 0 }
+end
+
+@storename = 'New West'
+
+Store.create(name: @storename, annual_revenue: 500000)
+
+p.errors.messages
