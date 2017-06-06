4b0d135f7e5897055033f6a793bfd2ab7e19e2a0
Alysia Lynn <itsalysialynn@gmail.com>
2017-06-06 15:37:19 -0700

    added one to many association

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,9 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store2.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Alysia", last_name: "Lynn", hourly_rate: 50)
+@store1.employees.create(first_name: "Alex", last_name: "Sia", hourly_rate: 70)
+@store2.employees.create(first_name: "Heather", last_name: "Hamilton", hourly_rate: 160)
+@store1.employees.create(first_name: "Jackie", last_name: "Jane", hourly_rate: 80)
+@store2.employees.create(first_name: "Jake", last_name: "Jones", hourly_rate: 40)
